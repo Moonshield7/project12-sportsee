2f4a88b727e84493253fe984f50593ef2cd2d372
Moonshield7 <fannie.p@hotmail.fr>
2023-10-22 13:06:59 +0200

    🎉 Starting the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LateralNavbar from './components/LateralNavbar'
+import TopNavbar from './components/TopNavbar'
+
+function App() {
+
+
+  return (
+      <div>
+        <TopNavbar />
+        <LateralNavbar />
+      </div>
+  )
+}
+
+export default App
