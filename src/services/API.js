5cfe0bdf19615fa15760f1ac184aa2665518248e
Moonshield7 <fannie.p@hotmail.fr>
2023-11-01 12:25:25 +0100

    ✨ Can fetch data from API or mock easily

--- src/services/API.js
+++ src/services/API.js
@@ -1,9 +1,17 @@
-
+const API_DOES_EXIST = true;
 
 async function getUser(idUser) {
-  const response = await fetch(`http://localhost:3000/user/${idUser}`);
-  const dataUser = await response.json();
-  const rawUser = dataUser.data;
+  let rawUser;
+  if(API_DOES_EXIST){
+    const response = await fetch(`http://localhost:3000/user/${idUser}`);
+    const dataUser = await response.json();
+    rawUser = dataUser.data;
+  } else {
+    const response = await fetch('/mock-data/data-users.json');
+    const dataUsers = await response.json();
+    rawUser = dataUsers.find(user => user.id == idUser);
+    console.log('User from mock');
+  }
   const formatedUser = [];
   
   if(rawUser.todayScore){
@@ -25,21 +33,48 @@ async function getUser(idUser) {
 }
 
 async function getActivity(idUser) {
-  const response = await fetch(`http://localhost:3000/user/${idUser}/activity`);
-  const dataActivity = await response.json();
-  return (dataActivity.data.sessions);
+  if(API_DOES_EXIST){
+    const response = await fetch(`http://localhost:3000/user/${idUser}/activity`);
+    const dataActivity = await response.json();
+    return (dataActivity.data.sessions);
+  } else {
+    const response = await fetch('/mock-data/data-activity.json');
+    const dataActivities = await response.json();
+    const activityUser = dataActivities.find(activity => activity.userId == idUser);
+    console.log('Sessions from mock');
+    return activityUser.sessions;
+  }
+
 }
 
 async function getAverageSession(idUser) {
-  const response = await fetch(`http://localhost:3000/user/${idUser}/average-sessions`);
-  const dataAverageSession = await response.json();
-  return (dataAverageSession.data.sessions);
+  if(API_DOES_EXIST){
+    const response = await fetch(`http://localhost:3000/user/${idUser}/average-sessions`);
+    const dataAverageSession = await response.json();
+    return (dataAverageSession.data.sessions);
+  } else {
+    const response = await fetch('/mock-data/data-session.json');
+    const dataAverageSessions = await response.json();
+    const AverageSessionsUser = dataAverageSessions.find(sessions => sessions.userId == idUser);
+    console.log('Average sessions from mock');
+    return AverageSessionsUser.sessions;
+  }
+
 }
 
 async function getPerformance(idUser) {
-  const response = await fetch(`http://localhost:3000/user/${idUser}/performance`);
-  const dataPerformance = await response.json();
-  return (dataPerformance.data.data);
+  if(API_DOES_EXIST){
+    const response = await fetch(`http://localhost:3000/user/${idUser}/performance`);
+    const dataPerformance = await response.json();
+    return (dataPerformance.data.data);
+  } else {
+    const response = await fetch('/mock-data/data-performance.json');
+    const dataPerformance = await response.json();
+    const PerformanceUser = dataPerformance.find(performance => performance.userId == idUser);
+    console.log('Average sessions from mock');
+    return PerformanceUser.data;
+  }
+
 }
 
 export {getUser, getActivity, getAverageSession, getPerformance};
