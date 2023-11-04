bcc7aeeb50b330d7a5c907ce1db56aadec2e4e13
Moonshield7 <fannie.p@hotmail.fr>
2023-11-04 10:30:31 +0100

    💄 Update of the tooltip from average session

--- src/components/AverageSessionDuration/index.jsx
+++ src/components/AverageSessionDuration/index.jsx
@@ -1,5 +1,5 @@
 import './style.css';
-import { LineChart, Line, XAxis, Tooltip, YAxis  } from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, YAxis, Rectangle  } from 'recharts';
 import PropTypes from 'prop-types';
 
 function AverageSessionDuration ({data}) {
@@ -17,10 +17,6 @@ function AverageSessionDuration ({data}) {
     return rawDatas;
   }
 
-  function CustomTooltip() {
-    return null;
-  }
-
   const formatedData = formatingData();
 
   return (
@@ -29,12 +25,15 @@ function AverageSessionDuration ({data}) {
       <LineChart width={260} height={260} data={formatedData}>
         <defs>
           <linearGradient id="colorUv" x1="0%" y1="0" x2="100%" y2="0">
-            <stop offset="30%" stopColor="#ffffff9c" stopOpacity={0.4} />
-            <stop offset="95%" stopColor="#ffffff9c" stopOpacity={1} />
+            <stop offset="0%" stopColor="#ffffff9c" stopOpacity={0.5} />
+            <stop offset="100%" stopColor="#ffffff9c" stopOpacity={1} />
           </linearGradient>
         </defs>
-        <Tooltip content={CustomTooltip} cursor={{ stroke: 'rgba(0, 0, 0, 0.1)', strokeWidth: 40}} />
-        <Line type="natural" dataKey="duration" stroke='url(#colorUv)' dot={false} strokeWidth={2} />
+        <Tooltip
+            content={<CustomTooltip />}
+            cursor={<CustomCursor />}
+          />
+        <Line type="natural" dataKey="duration" stroke='url(#colorUv)' dot={false} activeDot={false} strokeWidth={2} />
         <XAxis dataKey="dayInitial" padding={{ left: 10, right: 10 }} axisLine={false} tickLine={false} stroke='#ffffff9c' />
         <YAxis domain={['dataMin -15', 'dataMax +20']} hide={true} />
       </LineChart>
@@ -42,8 +41,40 @@ function AverageSessionDuration ({data}) {
   );
 }
 
+const CustomCursor = ({points}) => {
+    const X = points[0].x;
+    const Y = points[0].y;
+  
+    return (
+      <Rectangle
+        width={1000}
+        height={1000}
+        x={X - 20}
+        y={Y - 100}
+        style={{background: 'red', opacity: 0.1}}
+      />
+    );
+  };
+
+function CustomTooltip({ active, payload }) {
+  if (active && payload) {
+    return <p className='tooltip-container'>{`${payload[0].value} min`}</p>;
+  }
+
+  return null;
+}
+
 AverageSessionDuration.propTypes = {
   data: PropTypes.array,
+};
+
+CustomCursor.propTypes = {
+  points: PropTypes.array,
+};
+
+CustomTooltip.propTypes = {
+  active: PropTypes.bool,
+  payload: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default AverageSessionDuration;
